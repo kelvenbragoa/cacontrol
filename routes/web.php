2723d97f1a29760f15c1494560a40754defbaf0b
Kelven Bragoa <kelvenbragoa@hotmail.com>
2024-08-13 16:00:29 +0200

    commit

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,9 @@ Route::group(['middleware' => ['role:manager']], function () {
     
 });
 
-Route::middleware('auth')->group(function () {
+// Route::middleware('auth')->group(function () {
+Route::group(['middleware' => ['role:admin']], function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
